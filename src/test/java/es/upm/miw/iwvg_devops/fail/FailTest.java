b915fd912cc3a9ecd676920b0110eb3ab41705d0
Isabel Diaz <isabel.d.m@outlook.com>
2021-09-17 17:44:16 +0200

    Test Slack

---
+++ src/test/java/es/upm/miw/iwvg_devops/fail/FailTest.java
@@ -0,0 +1,12 @@
+package es.upm.miw.iwvg_devops.fail;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FailTest {
+    @Test
+    public void failTest(){
+        assertEquals(1,2);
+    }
+}
