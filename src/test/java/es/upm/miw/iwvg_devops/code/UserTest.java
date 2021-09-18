aa02d6396394ddea7c96936b30f6fcf054ddd6ba
Isabel Diaz <isabel.d.m@outlook.com>
2021-09-18 20:23:11 +0200

    Create Classes User and UserTest

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
@@ -0,0 +1,50 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void initUser() {
+        List<Fraction> list = List.of(
+                new Fraction(1, 5),
+                new Fraction(0, 4)
+        );
+
+        List<Fraction> fractions = new ArrayList<>(list);
+
+        this.user =
+                new User("1", "Sara", "Mendez", fractions);
+
+    }
+
+    @Test
+    void testGetUserFullName() {
+        assertEquals("Sara Mendez", user.fullName());
+    }
+
+    @Test
+    void testGetUserNameInitials() {
+        assertEquals("S.", user.initials());
+    }
+
+    @Test
+    void testAddFractionToUser() {
+        Fraction newFraction = new Fraction(1, 3);
+        user.addFraction(newFraction);
+        assertEquals(List.of(
+                new Fraction(1, 5),
+                new Fraction(0, 4),
+                newFraction),
+                user.getFractions());
+    }
+
+}
