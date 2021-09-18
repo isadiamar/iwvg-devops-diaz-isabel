ff6bc7829393ae348d0749a04c64d9cb3ee44b23
Isabel Diaz <isabel.d.m@outlook.com>
2021-09-18 18:48:17 +0200

    Create Classes Fraction and FractionTest

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -0,0 +1,60 @@
+package es.upm.miw.iwvg_devops.code;
+
+import java.util.Objects;
+
+public class Fraction {
+
+    private int numerator;
+
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return numerator == fraction.numerator && denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+}
